41c1eec40c1d6e0d428254852924158a5e5adcc2
princeallan <akirui@cytonn.com>
2018-09-17 17:38:04 +0000

    create product page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-4">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -18,6 +18,53 @@
                 </div>
             </div>
         </div>
+
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <strong>Create Product</strong></div>
+                    <div class="card-body">
+                        <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label" for="text-input">Product Name</label>
+                                <div class="col-md-9">
+                                    <input class="form-control" id="text-input" type="text" name="text-input" placeholder="Text">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label" for="select1">Select Category</label>
+                                <div class="col-md-9">
+                                    <select class="form-control" id="select1" name="select1">
+                                        <option value="0">Please select Category</option>
+                                        <option value="1">Product #1</option>
+                                        <option value="2">Product #2</option>
+                                        <option value="3">Product #3</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label" for="textarea-input">Product Description</label>
+                                <div class="col-md-9">
+                                    <textarea class="form-control" id="textarea-input" name="textarea-input" rows="9" placeholder="Content.."></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label" for="file-multiple-input">Product Images</label>
+                                <div class="col-md-9">
+                                    <input id="file-multiple-input" type="file" name="file-multiple-input" multiple="">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-sm btn-primary" type="submit">
+                            <i class="fa fa-dot-circle-o"></i> Submit</button>
+                        <button class="btn btn-sm btn-danger" type="reset">
+                            <i class="fa fa-ban"></i> Reset</button>
+                    </div>
+                </div>
+            </div>
+
     </div>
 </div>
 @endsection
